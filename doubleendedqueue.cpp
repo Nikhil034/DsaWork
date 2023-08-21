df4635eaf03ac260a2311356e40b1c14ea4b1550
Nikhil034 <nikhilsondarava342@gmail.com>
2023-08-21 22:36:41 +0530

    double ended queue

--- doubleendedqueue.cpp
+++ doubleendedqueue.cpp
@@ -1,152 +1,145 @@
 #include<iostream>
 #define size 5
-int f=-1;
-int r=-1;
+int f = -1;
+int r = -1;
 int queue[size];
 using namespace std;
 
-int isEmpty()
-{
-    if(f==-1 && r==-1 || f==size-1)
-    {
-       return 1;
-    }
-    else
-    {
-        return 0;
-    }
-}
-int isFull()
-{
-    if(f==0 && r==size-1)
-    {
+int isEmpty() {
+    if (f == -1 && r == -1 || f == r + 1) {
         return 1;
-    }
-    else
-    {
+    } else {
         return 0;
     }
 }
 
-void EnqueuFront()
-{
-    int val;
-    if(isFull())
-    {
-        cout<<"QUEUE IS ALREADY FULL!";
-    }
-    else
-    {
-        cout<<"Enter a value";
-        cin>>val;
-        r++;
-        queue[r]=val;
-
-        if(f==-1)
-        {
-            f=0;
-        }
-
+int isFull() {
+    if ((f == 0 && r == size - 1) || f == r + 1) {
+        return 1;
+    } else {
+        return 0;
     }
 }
-void EnqueuBack()
-{
+
+void EnqueuFront() {
     int val;
-    if(isFull())
-    {
-        cout<<"QUEUE IS ALREADY FULL!";
-    }
-    else
-    {
-        if(r==size-1 || f==r-1)
-        {
-           cout<<"NOT POSSIBLE";
-        }
-        else
-        {
-            cout<<"Enter a back value=";
-            cin>>val;
-            f=size-1;
-            queue[f]=val;
+    if (isFull()) {
+        cout << "QUEUE IS ALREADY FULL!";
+    } else {
+        cout << "Enter a value: ";
+        cin >> val;
+        if (f == -1) {
+            f = 0;
+            r = 0;
+        } else if (f == 0) {
+            f = size - 1;
+        } else {
             f--;
         }
-    }
-    f=0;
-}
-void DequeuFront()
-{
-    int delval;
-    if(isEmpty())
-    {
-        cout<<"QUEUE IS EMPTY!";
-    }
-    else
-    {
-        delval=queue[f];
-        cout<<"QUEUE VALUE FROM FRONT="<<delval;
-        f++;
+        queue[f] = val;
     }
 }
-void DequeueBack()
-{
-   int delval;
-   int tmp=r-1;
-   if(isEmpty())
-   {
-    cout<<"QUEUE IS EMPTY!";
-   }
-   else
-   { 
-    delval=queue[tmp];
-    cout<<"BACK VALUE="<<delval;
-    tmp--;
-   }
-   r=tmp;
-}
-void DisplayQueue()
-{
-    if(isEmpty())
-    {
-        cout<<"QUEUE IS EMPTY!";
+
+void EnqueuBack() {
+    int val;
+    if (isFull()) {
+        cout << "QUEUE IS ALREADY FULL!";
+    } else {
+        cout << "Enter a back value: ";
+        cin >> val;
+        if (r == -1) {
+            f = 0;
+            r = 0;
+        } else if (r == size - 1) {
+            r = 0;
+        } else {
+            r++;
+        }
+        queue[r] = val;
     }
-    else
-    {
-        for(int i=f;i<=r-1;i++)
-        {
-            cout<<queue[i]<<" | ";
+}
+
+void DequeuFront() {
+    if (isEmpty()) {
+        cout << "QUEUE IS EMPTY!";
+    } else {
+        cout << "QUEUE VALUE FROM FRONT=" << queue[f];
+        if (f == r) {
+            f = -1;
+            r = -1;
+        } else if (f == size - 1) {
+            f = 0;
+        } else {
+            f++;
         }
     }
 }
-int main(){
-    
+
+void DequeueBack() {
+    if (isEmpty()) {
+        cout << "QUEUE IS EMPTY!";
+    } else {
+        cout << "BACK VALUE=" << queue[r];
+        if (f == r) {
+            f = -1;
+            r = -1;
+        } else if (r == 0) {
+            r = size - 1;
+        } else {
+            r--;
+        }
+    }
+}
+
+void DisplayQueue() {
+    if (isEmpty()) {
+        cout << "QUEUE IS EMPTY";
+    } else {
+        int i = f;
+        while (true) {
+            cout << queue[i] << " | ";
+            if (i == r) {
+                break;
+            }
+            if (i == size - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+    }
+}
+
+int main() {
     int ch;
-    do
-    {
-        cout<<"\n 1.Enqueue Front \n 2.Enqueue Back \n 3.Dequeue Front \n 4.Dequeue Back \n 5.Display \n 0.Exit\n";
+    do {
+        cout << "\n 1.Enqueue Front \n 2.Enqueue Back \n 3.Dequeue Front \n 4.Dequeue Back \n 5.Display \n 0.Exit\n";
+        cout << "PASS YOUR USE CASE=";
+        cin >> ch;
 
-        cout<<"PASS YOUR USE CASE=";
-        cin>>ch;
-
-        switch (ch)
-        {
-        case 1:EnqueuFront();
-               break;
-        case 2:EnqueuBack();
-               break;     
-        case 3:DequeuFront();
-               break;       
-        case 4:DequeueBack();
-               break;
-        case 5:DisplayQueue();
-               break;
-        case 0:exit(0);                       
-        default:cout<<"CASE NOT FOUND!";
+        switch (ch) {
+        case 1:
+            EnqueuFront();
+            break;
+        case 2:
+            EnqueuBack();
+            break;
+        case 3:
+            DequeuFront();
+            break;
+        case 4:
+            DequeueBack();
+            break;
+        case 5:
+            DisplayQueue();
+            break;
+        case 0:
+            exit(0);
+        default:
+            cout << "CASE NOT FOUND!";
             break;
         }
-    
-    } while (ch!=0);
-    
-
+    } while (ch != 0);
 
     return 0;
 }
