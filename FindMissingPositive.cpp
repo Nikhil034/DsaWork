b20e32d91763ff1c69620682897c52fae782cc42
Nikhil034 <nikhilsondarava342@gmail.com>
2023-10-26 23:53:05 +0530

    Find missing number

---
+++ FindMissingPositive.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+int findSmallestMissingPositive(std::vector<int>& nums) {
+    int n = nums.size();
+    for (int i = 0; i < n; i++) {
+        while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+            std::swap(nums[i], nums[nums[i] - 1]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != i + 1) {
+            return i + 1;
+        }
+    }
+    return n + 1;
+}
+
+int main() {
+    std::vector<int> nums = {3, 4, -1, 1};
+    int result = findSmallestMissingPositive(nums);
+    std::cout << "The smallest missing positive integer is: " << result << std::endl;
+    return 0;
+}
