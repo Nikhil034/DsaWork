31d876e05a48fe65b1a6774262d87e0aba09dbac
Nikhil034 <nikhilsondarava342@gmail.com>
2023-12-26 18:46:59 +0530

    find an element from matrix

---
+++ Top_150_leetcode_81.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    if (matrix.empty() || matrix[0].empty()) {
+        return false;
+    }
+
+    int m = matrix.size();
+    int n = matrix[0].size();
+    int left = 0;
+    int right = m * n - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        int midElement = matrix[mid / n][mid % n];
+
+        if (midElement == target) {
+            return true;
+        } else if (midElement < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    vector<vector<int>> matrix = {
+        {1, 3, 5, 7},
+        {10, 11, 16, 20},
+        {23, 30, 34, 60}
+    };
+    int target = 3;
+
+    if (searchMatrix(matrix, target)) {
+        cout << "Target found in the matrix." << endl;
+    } else {
+        cout << "Target not found in the matrix." << endl;
+    }
+
+    return 0;
+}
